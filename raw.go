059cd66cb4aa039fe056acde81d92254c906e20d
Mark Karpeles <magicaltux@gmail.com>
2025-02-25 07:20:30 +0900

    make pjson.RawMessage equivalent to json.RawMessage

--- raw.go
+++ raw.go
@@ -1,28 +1,13 @@
 package pjson
 
-import "errors"
+import (
+	"encoding/json"
+)
 
 // RawMessage is a raw encoded JSON value.
 // It implements Marshaler and Unmarshaler and can
 // be used to delay JSON decoding or precompute a JSON encoding.
-type RawMessage []byte
-
-// MarshalJSON returns m as the JSON encoding of m.
-func (m RawMessage) MarshalJSON() ([]byte, error) {
-	if m == nil {
-		return []byte("null"), nil
-	}
-	return m, nil
-}
-
-// UnmarshalJSON sets *m to a copy of data.
-func (m *RawMessage) UnmarshalJSON(data []byte) error {
-	if m == nil {
-		return errors.New("json.RawMessage: UnmarshalJSON on nil pointer")
-	}
-	*m = append((*m)[0:0], data...)
-	return nil
-}
+type RawMessage = json.RawMessage
 
 var _ Marshaler = (*RawMessage)(nil)
 var _ Unmarshaler = (*RawMessage)(nil)
